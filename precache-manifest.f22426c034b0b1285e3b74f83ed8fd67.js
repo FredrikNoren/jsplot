cabffad72150015538580374afe4c2012f0ecb81
Fredrik Norén <fredrik.jw.noren@gmail.com>
2020-07-03 22:48:36 +0200

    Updates

---
+++ precache-manifest.f22426c034b0b1285e3b74f83ed8fd67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e453243c0b501947cc8dcbb6b060ebe",
+    "url": "/jsplot/index.html"
+  },
+  {
+    "revision": "64c2ccc7624f39249860",
+    "url": "/jsplot/static/css/2.932f93f0.chunk.css"
+  },
+  {
+    "revision": "5c0b737757a7254f336f",
+    "url": "/jsplot/static/css/main.26a763d8.chunk.css"
+  },
+  {
+    "revision": "64c2ccc7624f39249860",
+    "url": "/jsplot/static/js/2.a75721d2.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/jsplot/static/js/2.a75721d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c0b737757a7254f336f",
+    "url": "/jsplot/static/js/main.0f7c15d5.chunk.js"
+  },
+  {
+    "revision": "e993c8797b32442197e0",
+    "url": "/jsplot/static/js/runtime-main.5117add3.js"
+  }
+]);
